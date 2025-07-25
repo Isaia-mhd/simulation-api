5105efa9ed6614dfb5cd2cac46a1113884707e74
Isaia-mhd <mohamedesaie21@gmail.com>
2025-07-25 21:49:20 +0300

    feat: add landing - lighting(balisage) - approach model and migrations

---
+++ database/migrations/2025_07_25_183532_create_lightings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lightings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("airplane_id")->constrained();
+            $table->decimal("adema", 10, 2)->nullable()->default(0);
+            $table->decimal("asecna", 10, 2)->nullable()->default(0);
+            $table->decimal("ravinala", 10, 2)->nullable()->default(0);
+            $table->decimal("total_lighting", 10, 2)->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lightings');
+    }
+};
