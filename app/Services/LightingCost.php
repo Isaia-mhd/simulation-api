7c98572841e6f54eaf41c8a0ab0338a575400c81
Isaia-mhd <mohamedesaie21@gmail.com>
2025-07-27 04:26:33 +0300

    fix(Lighting): make some logic with sunraisetime and sunsettime

--- app/Services/LightingCost.php
+++ app/Services/LightingCost.php
@@ -5,16 +5,43 @@ namespace App\Services;
 use App\Models\Airplane;
 use App\Models\Airport;
 use App\Models\Lighting;
+use App\Models\SunriseSunsetTime;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class LightingCost
 {
-    public function lightingCost(array $cost)
+    public function lightingCost(array $flight)
     {
         //get the which airplane is the flight
-        $airplane = Airplane::find($cost["airplane_id"]);
+        $airplane = Airplane::find($flight["airplane_id"]);
 
         //and what is the airport destination
-        $airportCode = Airport::where("id", $cost["arrival_airport_id"])->value("code");
+        $airportCode = Airport::where("id", $flight["arrival_airport_id"])->value("code");
+
+        $estimatedArrival = new  EstimatedArrival();
+        $arrivalDateTime = $estimatedArrival->estimateTime($flight);
+
+        $date = Carbon::parse($arrivalDateTime);
+        $timeOnly = $date->format('H:i'); // Résultat : "18:05"   2025-07-27 17:21:58
+
+        $month = $date->format('m');
+        $day = $date->format('d');
+
+        $sunTiming = SunriseSunsetTime::where("month", intval($month))
+                                        ->where("day", intval($day))
+                                        ->where("airport_code", strtolower($airportCode))
+                                        ->first();
+
+
+        $sunRaise = $sunTiming->sunrise_time;
+        $sunset = $sunTiming->sunset_time;
+
+        //3:20              < 18:05            <     14:40            18:05
+        if ($sunRaise < $timeOnly && $timeOnly < $sunset)
+        {
+            return 0.00;
+        }
 
         //fetch it and return its total
         $lighting = Lighting::where("airplane_name", $airplane->name)
--- database/migrations/2025_07_24_191058_create_airports_table.php
+++ database/migrations/2025_07_24_191058_create_airports_table.php
@@ -16,8 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string("code");
             $table->boolean("has_lighting")->default(true);
-            $table->time("sunset_time")->nullable();
-            $table->string("fuel_price");
+            $table->string("fuel_price")->nullable()->default(null);
             $table->string("unit")->default("MGA/L");
             $table->timestamps();
         });
