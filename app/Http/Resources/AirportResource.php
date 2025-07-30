5c16ff6358011a1194e11de105a23133a7956e61
Isaia-mhd <mohamedesaie21@gmail.com>
2025-07-30 20:33:18 +0300

    fix: add the id of airport in AirportResourcce

--- app/Http/Resources/AirportResource.php
+++ app/Http/Resources/AirportResource.php
@@ -15,6 +15,7 @@ class AirportResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "name" => $this->name,
             "code" => $this->code,
             "fuel_price" => $this->fuel_price,
