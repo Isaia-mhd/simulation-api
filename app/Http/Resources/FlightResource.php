5a71a7dcc3fe5cfb22b9207872fcfd61ec39a74f
Isaia-mhd <mohamedesaie21@gmail.com>
2025-07-31 07:09:25 +0300

    fix: add some attribute in FlightResourcce

--- app/Http/Resources/FlightResource.php
+++ app/Http/Resources/FlightResource.php
@@ -21,30 +21,40 @@ class FlightResource extends JsonResource
 
         $baseCost = $this->base_cost ?? [];
 
-        $revenue = $economy->sum("ticket_price") + $business->sum("ticket_price");
-
         $currency = config("services.exchanges.default_rate_mga_eur"); // EUR
+
+        $revenue = ($economy->sum("ticket_price") + $business->sum("ticket_price")) * $currency;
+
+
 
         return [
             "id" => $this->id,
             "name" => $this->name,
+            "departure_date" => $this->departure_date,
             "estimated_arrival_date" => $this->estimated_arrival_date,
+            "status" => $this->status,
             "created_at" => $this->created_at,
+            "currency" => [
+                "EUR" => $currency,
+                "USD" => $currency,
+            ],
             "passenger" => [
                 "count" => $passengers->count(),
                 "economy" => [
                     "total" => $economy->count(),
                     "ticket_price_eur" => (float) $economy->value("ticket_price"),
-                    "ticket_price" => $economy->value("ticket_price") * $currency,
+                    "ticket_price_mga" => $economy->value("ticket_price") * $currency,
+                    "total_ticket_price_eur" => $economy->value("ticket_price") * $economy->count(),
                     "total_price" => ($economy->value("ticket_price") * $currency) * $economy->count(),
                 ],
                 "business" => [
                     "total" => $business->count(),
                     "ticket_price_eur" => (float) $business->value("ticket_price"),
-                    "ticket_price" => $business->value("ticket_price") * $currency,
+                    "ticket_price_mga" => $business->value("ticket_price") * $currency,
+                    "total_ticket_price_eur" => (float) $business->value("ticket_price") * $business->count(),
                     "total_price" => ($business->value("ticket_price") * $currency) * $business->count(),
                 ],
-                "revenue" => round($revenue, 2),
+                "revenue" => round($revenue, 2) ,
             ],
             "base_cost" => $baseCost,
             'airplane' => $this->whenLoaded('airplane'),
